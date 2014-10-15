de7599c01e9c4f571225d01eae9813f017e84344
caf318 <caf318@lehigh.edu>
2014-10-15 01:56:24 +0000

    Updated hw06

--- hw06/Root.java
+++ hw06/Root.java
@@ -2,6 +2,7 @@
 //hw06
 //Root
 //10-13-14
+//                 This program uses the bisection method  to find the square root of a number that the user inputs.
 //
 //      javac Root.java
 //
@@ -17,42 +18,42 @@ public class Root{
     Scanner b;
     b =new Scanner(System.in);
  
-      System.out.println("Please enter an integer: "); //prompts user
+      System.out.println("Please enter an integer: ");     //prompts user
     
-    int x = b.nextInt(); // declaration of user input
+    int x = b.nextInt();                                   // declaration of user input
     
-    if( x < 0){  // if input is below 0 then the program restarts
-            return;   // restart
-    }  // end of if
+    if( x < 0){                                            // if input is below 0 then the program restarts
+            return;                                        // restart
+    }                                                      // end of if
     
-   else{  // beggining of else statement
+   else{                                                   // beggining of else statement
      
-    double low = 0;    // low is declared 
-    double high = 1 + x;  // high is declared
-    double middle = ((high + low)/2.00);  // find the middle of the two terms
+    double low = 0;                                        // low is declared 
+    double high = 1 + x;                                   // high is declared
+    double middle = ((high + low)/2.00);                   // find the middle of the two terms
   
-   while ( high-low > .0000001*(1+x) ){  //while loop begins
+   while ( high-low > .0000001*(1+x) ){                    //while loop begins
     
-    if ( middle * middle > x){   // if middle squared is greater than zero then high becomes middle
+    if ( middle * middle > x){                             // if middle squared is greater than zero then high becomes middle
      
-    high = middle;  // high becomes middle
+    high = middle;                                         // high becomes middle
     }
-     else {               // start of else
-         low = middle; // low becomes middle
-     } //  end of else
+     else {                                                // start of else
+         low = middle;                                     // low becomes middle
+     }                                                     //  end of else
   
     
-   middle = ((high + low)/2.00); // since high and low becom middle you must repeat the process
+   middle = ((high + low)/2.00);                           // since high and low becom middle you must repeat the process
    
     
-   }// end of while loop
+   }                                                       // end of while loop
     
        
-     System.out.println( "The root is " + middle); // the output
+     System.out.println( "The root is " + middle);         // the output
     
-       } // end of else
-    } // end of method
-  } // end of class
+       }                                                   // end of else
+    }                                                      // end of method
+  }                                                        // end of class
 
     
     
--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -2,61 +2,72 @@
 //hw06
 //Roulette
 //10-13-14
+//  This program is supposed to simulate a roulette game. A person is betting on 100 spins, however thusis simulation runs 1000 times
+// and collects the data of the income.
 //
 //      javac Roulette.java
 //
 //      java Roulette//
 
-      
+      import java.util.Scanner;                           // imports scanner
 
 public class Roulette{                                      // class
 
   public static void main(String[] args){                 // main method
    
    
-
-       int simulation=0;                              // simulation begins at 0
+   Scanner myScanner = new Scanner( System.in);           // allows input
    
+  System.out.print(" Please enter an integer between 1 and 38: " ); //prompts the user
+  
+      int bet = myScanner.nextInt();                 // user input or bet
+      int simulation=1;                              // simulation begins at 0
+      int totalwins =0;                              // initial amount of total wins
+      int lose = 0;                                  // initial amount of losses
+      int reward = 0;                                // the money you will earn from winning
+  
+      if( bet <38 && bet > 0){
+          
   while( simulation <= 1000){                         // the beginning of the 1000 repitions loop
    
       int wins = 0;                                   // intial amount of wins
-      int totalwins =0;                                // initial amount of total wins
-      int lose = 0;                                   // initial amount of losses
       int number = (int)(Math.random()*38);           // random number
-      int bet = (int)(Math.random()*38);              // the bet
-      int rolls = 1;                                 // starting amount of rolls if zero you would be playing 101 rolls
+      int rolls = 1;                                  // starting amount of rolls if zero you would be playing 101 rolls
       
       
-   while ( rolls <=100){                             // limits the amount of rolls taken
+  while ( rolls <=100){                                 // while loop that states the amount of rolls should be no greater than 100
    
-   if( number == bet){                               // if the bet is equal to random the
-    wins++;                                            // a win is counted and added
-    totalwins++;                                      // total wins is counted 
+      if( number == bet){                               // if the bet and random number equal one another 
+         totalwins++;                                   // then the amount of total wins increase by 1
+         wins++;                                        // the amount of wins increase by 1
    }
    
-   rolls++; }                                       // end of loop and end of roll
+    rolls++; }                                          // the amount of rolls increase by 1
    
-    if (wins == 0){                                 // now focus on wins, if wins is 0 then loses are added
-     lose++;
-     System.out.println("You lost money and the bet"); 
+    if (wins == 0){                                     // the conditon stating that if wins is equal to zero then...
+     lose++;                                            // the losses add onto each other
      }
      
-    else if (wins >= 3){                              // if you win once or more than once then...
-     int reward = 36*wins;                           // the money you will earn from winning will be calculated
-      reward++;                                        // reward is added up 
-     System.out.println("You have won $" + reward);                 // results
-      System.out.println("You have won " + totalwins + " times");      // results
-     
+    else if (wins >= 1){                                // another possible answer if wins are not 0
+      
+      reward++;                                         // rewards continue to be added only if the are more than one wins
      }
      
-     simulation++; // the simulation continues to loop until it reaches 1000
+     simulation++;                                      // the simulation continues to loop until it reaches 1000
    
-  } // end of simulation loop
+  }                                                     // end of simulation loop
   
-  } // end of method
+       System.out.println("You have won $" + reward*36);            //result
+       System.out.println("You have won " + totalwins + " times");  //result
+       System.out.println("You lost " + lose + " times");           //result
+   
+  }                                                     // end of if statement
   
-} // end of class
+    else {                                              // else statement begins
+       System.out.println("You have not entered a number between 1 and 38");    // statement that prints if the number isnt in the range
+   }                                                    // end of else
   
-
+  }                                                     // end of method
   
+}                                                       // end of class
   
